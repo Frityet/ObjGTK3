3f8b366a8b35aa1fa397601e0e11b6f449f56532
Amrit Bhogal <ambhogal01@gmail.com>
2024-04-07 15:22:59 -0700

    use extsources

--- packages.lua
+++ packages.lua
@@ -2,37 +2,27 @@ package("gio")
     set_homepage("https://docs.gtk.org/gio/")
     set_description("Useful classes for general purpose I/O, networking, IPC, settings, and other high level application functionality")
     set_license("LGPL-2.1")
+    add_extsources("pkgconfig::gio-2.0")
+    add_extsources("pkgconfig::gio")
 
-    on_fetch(function (package, opt)
-        if opt.system and package.find_package then
-            local result = package:find_package("gio-2.0")
-            if not result then result = package:find_package("gio") end
-            return result
-        end
-    end)
+    add_deps("glib")
+
 
 package("gio-unix")
     set_homepage("https://docs.gtk.org/gio/")
     set_description("Useful classes for general purpose I/O, networking, IPC, settings, and other high level application functionality")
     set_license("LGPL-2.1")
+    add_extsources("pkgconfig::gio-unix-2.0")
+    add_extsources("pkgconfig::gio-unix")
 
-    on_fetch(function (package, opt)
-        if opt.system and package.find_package then
-            local result = package:find_package("gio-unix-2.0")
-            if not result then result = package:find_package("gio-unix") end
-            return result
-        end
-    end)
+    add_deps("glib", "gio")
 
 package("atk")
     set_homepage("https://docs.gtk.org/atk/")
     set_description("Accessibility Toolkit")
     set_license("LGPL-2.1")
+    add_extsources("pkgconfig::atk-1.0")
+    add_extsources("pkgconfig::atk")
 
-    on_fetch(function (package, opt)
-        if opt.system and package.find_package then
-            local result = package:find_package("atk-1.0")
-            if not result then result = package:find_package("atk") end
-            return result
-        end
-    end)
+    add_deps("glib")
+
